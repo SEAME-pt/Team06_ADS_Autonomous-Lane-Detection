afd34209e84fa8aa16462852cd3013d721b01e0d
SomeMedtnerGuy <nuno.sousa@seame.pt>
2025-04-26 17:14:09 +0100

    26/04--1714

--- object_detection/test_img_trt.py
+++ object_detection/test_img_trt.py
@@ -1,104 +1,66 @@
-""" import cv2
-import numpy as np
-import pycuda.driver as cuda
-import pycuda.autoinit
-import tensorrt as trt
-
-TRT_LOGGER = trt.Logger()
-
-def load_engine(engine_path):
-    with open(engine_path, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
-        engine = runtime.deserialize_cuda_engine(f.read())
-    return engine
-
-def preprocess(img, input_size):
-    img_resized = cv2.resize(img, input_size)
-    img_rgb = img_resized[:, :, ::-1]
-    img_transposed = img_rgb.transpose(2, 0, 1)  # HWC ➝ CHW
-    img_norm = img_transposed.astype(np.float32) / 255.0
-    return np.ascontiguousarray(img_norm)
-
-def infer(context, inputs, outputs, bindings, stream):
-    cuda.memcpy_htod_async(inputs[0]['device'], inputs[0]['host'], stream)
-    context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
-    cuda.memcpy_dtoh_async(outputs[0]['host'], outputs[0]['device'], stream)
-    stream.synchronize()
-    return outputs[0]['host']
-
-# Caminho do modelo
-engine_path = "best.engine"
-engine = load_engine(engine_path)
-context = engine.create_execution_context()
-
-# Input info
-input_binding_idx = engine.get_tensor_index("images")
-output_binding_idx = engine.get_binding_index("output0")
-input_shape = engine.get_binding_shape(input_binding_idx)
-input_size = (input_shape[-1], input_shape[-2])  # (W, H)
-
-# Alocar buffers
-inputs = [{'host': cuda.pagelocked_empty(trt.volume(input_shape), dtype=np.float32),
-           'device': cuda.mem_alloc(trt.volume(input_shape) * 4)}]
-outputs = [{'host': cuda.pagelocked_empty(trt.volume(engine.get_binding_shape(output_binding_idx)), dtype=np.float32),
-            'device': cuda.mem_alloc(trt.volume(engine.get_binding_shape(output_binding_idx)) * 4)}]
-bindings = [int(inputs[0]['device']), int(outputs[0]['device'])]
-stream = cuda.Stream()
-
-# === Testar com uma imagem ===
-img_path = "exemplo.jpg"
-img = cv2.imread(img_path)
-img_input = preprocess(img, input_size)
-inputs[0]['host'] = img_input.ravel()
-
-output = infer(context, inputs, outputs, bindings, stream)
-
-# Exibir resultado bruto (para debug)
-print("Output shape:", output.shape)
-print("Output data (primeiros 10 valores):", output[:10]) """
-
-
-import tensorrt as trt
-import pycuda.driver as cuda
-import pycuda.autoinit
-import numpy as np
 import cv2
+import numpy as np
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
+
+# Inicializar TensorRT
+TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
+engine_file_path = "best.engine"
 
 # Carregar o engine
-TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
-with open("best.engine", "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
-    engine = runtime.deserialize_cuda_engine(f.read())
+with open(engine_file_path, "rb") as f:
+    engine_data = f.read()
 
-# DEBUG: listar todos os bindings
-for i in range(engine.num_bindings):
-    name = engine.get_binding_name(i)
-    print(f"Binding {i}: {name}")
-
-# Definir o binding correto manualmente
-input_binding_idx = 0  # ou outro número, dependendo do que aparecer
-
-# Criar context
+runtime = trt.Runtime(TRT_LOGGER)
+engine = runtime.deserialize_cuda_engine(engine_data)
 context = engine.create_execution_context()
 
-# Preparar entrada
-img = cv2.imread("image.jpg")  # <- teu caminho de teste
-img = cv2.resize(img, (640, 640))
-img = img.transpose((2, 0, 1))  # channels first
-img = np.expand_dims(img, axis=0).astype(np.float32) / 255.0  # Normalizar, se necessário
+# Descobrir input e output bindings para TensorRT 10
+for idx in range(engine.num_io_tensors):
+    tensor_name = engine.get_tensor_name(idx)
+    tensor_shape = engine.get_tensor_shape(tensor_name)
+    tensor_dtype = engine.get_tensor_dtype(tensor_name)
+    tensor_mode = engine.get_tensor_mode(tensor_name)
 
-# Alocar buffers
-d_input = cuda.mem_alloc(img.nbytes)
-d_output = cuda.mem_alloc(1000 * 4)  # tamanho aproximado, ajustar se souber
+    if tensor_mode == trt.TensorIOMode.INPUT:
+        input_name = tensor_name
+        input_shape = tensor_shape
+    else:
+        output_name = tensor_name
+        output_shape = tensor_shape
 
-bindings = [int(d_input), int(d_output)]
+# ---- PRE-PROCESSAR IMAGEM ----
+img = cv2.imread('cao_homem.jpg')
+img_resized = cv2.resize(img, (640, 640))  # Resize para o esperado
+img_rgb = cv2.cvtColor(img_resized, cv2.COLOR_BGR2RGB)
+img_normalized = img_rgb.astype(np.float32) / 255.0
+img_transposed = np.transpose(img_normalized, (2, 0, 1))
+img_input = np.expand_dims(img_transposed, axis=0)  # (1, 3, 640, 640)
+img_input = np.ascontiguousarray(img_input)
 
-# Copiar dados de input
-cuda.memcpy_htod(d_input, img)
+# ---- ALOCAR MEMÓRIA CUDA ----
+d_input = cuda.mem_alloc(img_input.nbytes)
+d_output = cuda.mem_alloc(np.empty(output_shape, dtype=np.float32).nbytes)
 
-# Inference
-context.execute_v2(bindings)
+# ---- PREPARAR STREAM e BINDINGS ----
+stream = cuda.Stream()
+cuda.memcpy_htod_async(d_input, img_input, stream)
 
-# Pegar output
-output = np.empty([1000], dtype=np.float32)  # ajustar tamanho se necessário
-cuda.memcpy_dtoh(output, d_output)
+context.set_tensor_address(input_name, int(d_input))
+context.set_tensor_address(output_name, int(d_output))
 
-print("Output:", output)
+context.execute_async_v3(stream.handle)
+
+# ---- COPIAR OUTPUT para CPU ----
+output = np.empty(output_shape, dtype=np.float32)
+cuda.memcpy_dtoh_async(output, d_output, stream)
+
+# ---- ESPERAR STREAM CONCLUIR ----
+stream.synchronize()
+
+# ---- OUTPUT PRONTO ----
+print("Output shape:", output.shape)
+print("Output sample:", output.flatten()[:10])
+
+
