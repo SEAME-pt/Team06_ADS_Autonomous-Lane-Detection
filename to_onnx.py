63f1da9265cda63bc5cd64fb7df9df58a87717af
akadjoker <djokertheripper@gmail.com>
2025-05-30 15:12:58 +0100

    add onxx and converter

---
+++ to_onnx.py
@@ -0,0 +1,35 @@
+import torch
+import torch.onnx
+from model import TwinLite as net
+import numpy as np
+
+def convert_to_onnx():
+ 
+    model = net.TwinLiteNet()
+    model = model.cuda()
+    model.load_state_dict(torch.load('pretrained/model.pth'))
+    model.eval()  
+    
+    
+    dummy_input = torch.randn(1, 3, 360, 640).cuda()
+     
+    torch.onnx.export(
+        model,                          
+        dummy_input,                    # entrada dummy
+        "model.onnx",      # nome do arquivo de saída
+        export_params=True,             # armazenar pesos treinados
+        opset_version=11,               # versão ONNX
+        do_constant_folding=True,       # otimização
+        input_names=['input'],          # nomes das entradas
+        output_names=['drivable_area', 'lane_lines'],  # nomes das saídas
+        dynamic_axes={
+            'input': {0: 'batch_size'},
+            'drivable_area': {0: 'batch_size'},
+            'lane_lines': {0: 'batch_size'}
+        }
+    )
+    print("Modelo convertido para ONNX com sucesso!")
+
+if __name__ == "__main__":
+    convert_to_onnx()
+
