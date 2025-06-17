3ff79cb4f75278fed11c99b6dfe59130c5ac3c2d
BelmiroRodrigues <barodrigues@outlook.pt>
2025-06-17 15:30:33 +0200

    new model 448

--- MPC/cpp_lane_infer_v6_nmpc/build/CMakeFiles/lane_detection.dir/DependInfo.cmake
+++ MPC/cpp_lane_infer_v6_nmpc/build/CMakeFiles/lane_detection.dir/DependInfo.cmake
@@ -1,18 +1,24 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/FServo/FServo.cpp" "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/build/CMakeFiles/lane_detection.dir/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/FServo/FServo.cpp.o"
+  "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/lane_detection.cpp" "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/build/CMakeFiles/lane_detection.dir/lane_detection.cpp.o"
+  "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/main.cpp" "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/build/CMakeFiles/lane_detection.dir/main.cpp.o"
+  "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/mask_processor.cpp" "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/build/CMakeFiles/lane_detection.dir/mask_processor.cpp.o"
+  "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/nmpc_controller.cpp" "/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/build/CMakeFiles/lane_detection.dir/nmpc_controller.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/team06/code/Team06-ADS_Autonomous-Lane-Detection/MPC/FServo/FServo.cpp" "CMakeFiles/lane_detection.dir/home/team06/code/Team06-ADS_Autonomous-Lane-Detection/MPC/FServo/FServo.cpp.o" "gcc" "CMakeFiles/lane_detection.dir/home/team06/code/Team06-ADS_Autonomous-Lane-Detection/MPC/FServo/FServo.cpp.o.d"
-  "/home/team06/code/Team06-ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/lane_detection.cpp" "CMakeFiles/lane_detection.dir/lane_detection.cpp.o" "gcc" "CMakeFiles/lane_detection.dir/lane_detection.cpp.o.d"
-  "/home/team06/code/Team06-ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/main.cpp" "CMakeFiles/lane_detection.dir/main.cpp.o" "gcc" "CMakeFiles/lane_detection.dir/main.cpp.o.d"
-  "/home/team06/code/Team06-ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/mask_processor.cpp" "CMakeFiles/lane_detection.dir/mask_processor.cpp.o" "gcc" "CMakeFiles/lane_detection.dir/mask_processor.cpp.o.d"
-  "/home/team06/code/Team06-ADS_Autonomous-Lane-Detection/MPC/cpp_lane_infer_v6_nmpc/nmpc_controller.cpp" "CMakeFiles/lane_detection.dir/nmpc_controller.cpp.o" "gcc" "CMakeFiles/lane_detection.dir/nmpc_controller.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/src/tensorrt/include"
+  "../"
+  "../../FServo"
+  "/usr/include/opencv4"
   )
 
 # Targets to which this target links.
--- MPC/cpp_lane_infer_v6_nmpc/build/CMakeFiles/lane_detection.dir/cmake_clean.cmake
+++ MPC/cpp_lane_infer_v6_nmpc/build/CMakeFiles/lane_detection.dir/cmake_clean.cmake
@@ -1,14 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/lane_detection.dir/home/team06/code/Team06-ADS_Autonomous-Lane-Detection/MPC/FServo/FServo.cpp.o"
-  "CMakeFiles/lane_detection.dir/home/team06/code/Team06-ADS_Autonomous-Lane-Detection/MPC/FServo/FServo.cpp.o.d"
+  "CMakeFiles/lane_detection.dir/home/jetson/Desktop/Team06/Team06_ADS_Autonomous-Lane-Detection/MPC/FServo/FServo.cpp.o"
   "CMakeFiles/lane_detection.dir/lane_detection.cpp.o"
-  "CMakeFiles/lane_detection.dir/lane_detection.cpp.o.d"
   "CMakeFiles/lane_detection.dir/main.cpp.o"
-  "CMakeFiles/lane_detection.dir/main.cpp.o.d"
   "CMakeFiles/lane_detection.dir/mask_processor.cpp.o"
-  "CMakeFiles/lane_detection.dir/mask_processor.cpp.o.d"
   "CMakeFiles/lane_detection.dir/nmpc_controller.cpp.o"
-  "CMakeFiles/lane_detection.dir/nmpc_controller.cpp.o.d"
   "lane_detection"
   "lane_detection.pdb"
 )
